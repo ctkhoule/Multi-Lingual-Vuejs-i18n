6859a99b93276530f0a0aeb96f5f9f8df7c750e6
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-01-23 16:04:48 +0000

    Init projet

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Cheikh TK - Multi langue vuejs"/>
   </div>
 </template>
 
