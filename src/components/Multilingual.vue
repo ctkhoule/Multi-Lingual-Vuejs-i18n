0f36a23374eafcf0ce6e2c92f730b874d8d79328
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-01-23 18:01:10 +0000

    Create component Multilingual

---
+++ src/components/Multilingual.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        {{ $t('message.value', { value: 'This is an exemple of content translate' }) }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Multilingual'
+}
+</script>
